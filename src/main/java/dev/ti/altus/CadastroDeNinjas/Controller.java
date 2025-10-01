dd08a226bf85e020aa5897b8ed813dbda9ce01ab
Pedro Nascimento <pedro.nascimento.ext@sodexo.com>
2025-10-01 01:17:20 -0300

    Primeira rota (teste)

---
+++ src/main/java/dev/ti/altus/CadastroDeNinjas/Controller.java
@@ -0,0 +1,16 @@
+package dev.ti.altus.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boas-vindas")
+    public String boasVindas() {
+        return "Essa é minha primeira mensagem nessa rota";
+    }
+
+}
